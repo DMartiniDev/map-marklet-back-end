035155189ef5badd77aff2868aa1bba48ea634c6
Dave Martinez <dkm.coder@gmail.com>
2018-04-30 16:03:41 +0200

    Refactor: Change code to use 'dotenv' and use environment variables

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 var admin = require("firebase-admin");
-var serviceAccount = require("./privateKey.json");
+require('dotenv').config()
 const app = express();
 
-var port = process.env.PORT || 3000;
+var serviceAccount = {
+  "type": process.env.type,
+  "project_id": process.env.project_id,
+  "private_key_id": process.env.private_key_id,
+  "private_key":  process.env.private_key,
+  "client_email":  process.env.client_email,
+  "client_id":  process.env.client_id,
+  "auth_uri":  process.env.auth_uri,
+  "token_uri":  process.env.token_uri,
+  "auth_provider_x509_cert_url":  process.env.auth_provider_x509_cert_url,
+  "client_x509_cert_url":  process.env.client_x509_cert_url
+}
+
+var port = process.env.PORT;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
