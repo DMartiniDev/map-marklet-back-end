0bf2d4e7524bc6044432c8d9ce9f98d7ebc5eea5
Dave Martinez <dkm.coder@gmail.com>
2018-04-30 11:01:32 +0200

    Feat: Retrieve all user markers from the Firebase database

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express');
+var admin = require("firebase-admin");
+var serviceAccount = require("./privateKey.json");
 const app = express();
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://map-marklet-b068f.firebaseio.com"
+});
+
+// Retrieve services
+defaultAuth = admin.auth();
+defaultDatabase = admin.database();
+
 app.get('/:user', function (req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify({
-    user: req.params.user,
-    query: req.query
-  }));
+  defaultDatabase.ref().child('users').child(req.params.user).once("value", data => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data.val());
+  });
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
